e2ce7631a3262ed110a8dfba132e1eda216d3125
Xalsier <81521840+Xalsier@users.noreply.github.com>
2024-10-19 04:29:36 -0400

    Updates to css and ekg

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const eunUrl = 'https://raw.githubusercontent.com/Xalsier/blood_Inv/refs/heads/main/eun.json';
+const debugUrl = 'https://raw.githubusercontent.com/Xalsier/blood_Inv/refs/heads/main/debug.json';
+
+let currentDataUrl = eunUrl; // Default to Eun's data
